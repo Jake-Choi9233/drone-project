558001acfed336b7d8330446e7945c9dd7b09c4f
Jake Choi <62556343+Jake-Choi9233@users.noreply.github.com>
2025-06-22 20:28:19 +0900

    Add files via upload
    flask socket upload

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route("/")
 def index():
     return render_template("chart.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    socketio.run(app, host="0.0.0.0", port=5000)
